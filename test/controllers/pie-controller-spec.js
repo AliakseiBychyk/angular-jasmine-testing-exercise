6d0479d5d354c1c59553e805d386b76cae859c37
Aliaksei Bychyk <aliaksei_bychyk@epam.com>
2017-02-07 09:52:24 +0300

    controller watchers testing

--- test/controllers/pie-controller-spec.js
+++ test/controllers/pie-controller-spec.js
@@ -13,6 +13,54 @@ describe("PieController", function() {
         });
     });
 
+    describe("Watchers", function() {
+
+        beforeEach(function() {
+            $scope.$digest();
+        });
+
+        describe("nutritionalValue", function() {
+
+            it("Should set the warning that Carbs have gone up, when only Carbs go up", function() {
+                $scope.nutritionalValue.carbs++;
+                $scope.$digest();
+                expect($scope.warning).toEqual("Carbs have gone up!");
+            });
+
+            it("Should set the warning that Fat have gone up, when only Fat go up", function() {
+                $scope.nutritionalValue.fat++;
+                $scope.$digest();
+                expect($scope.warning).toEqual("Fat have gone up!");
+            });
+
+            it("Should set the warning that Calories have gone up, when only calories go up", function() {
+                $scope.nutritionalValue.calories++;
+                $scope.$digest();
+                expect($scope.warning).toEqual("Calories have gone up!");
+            });
+
+            it("Should set the warning that combination has gone up, when only carbs go up", function() {
+                $scope.nutritionalValue.carbs++;
+                $scope.nutritionalValue.fat++;
+                $scope.nutritionalValue.calories++;
+                $scope.$digest();
+                expect($scope.warning).toEqual("Calories, Fat, Carbs have gone up!");
+            });
+
+            it("Should set the warning to null, when nothing goes up", function() {
+                expect($scope.warning).toBeNull();
+            });
+
+            it("Should set the warning to nul, when nothing has gone up, even if some things have done", function() {
+                $scope.nutritionalValue.carbs--;
+                $scope.nutritionalValue.fat--;
+                $scope.nutritionalValue.calories--;
+                $scope.$digest();
+                expect($scope.warning).toBeNull();
+            });
+        });
+    });
+
     describe("Action Handlers", function() {
 
         describe("eatSlice", function() {
@@ -38,11 +86,17 @@ describe("PieController", function() {
     });
 
     describe("Initialization", function() {
+        it("Should instantiate nutritionalValue to its default", function() {
+            expect($scope.nutritionalValue).toEqual({ calories: 500, fat: 200, carbs: 100 });
+        });
+        it("Should instantiate warning to null", function() {
+            expect($scope.warning).toEqual(null);
+        });
         it("Should instantiate slices to 8", function() {
             expect($scope.slices).toEqual(8);
         });
         it("Should instantiate $scope.lastRequestedFlavor", function() {
             expect($scope.lastRequestedFlavor).toBeUndefined();
-        })
+        });
     });
 });
